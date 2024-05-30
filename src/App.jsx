f1ccf3ad155808640362fd4534ce86d7bea3612c
AnnaFedorovaGit <ann.petrovska94@gmail.com>
2024-05-30 22:30:33 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,36 @@ import { Route, Routes } from 'react-router-dom';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
 import FirstPage from 'pages/FirstPage/FirstPage';
 import SecondPage from 'pages/SecondPage/SecondPage';
-import HalfPage from 'pages/HalfPage/HalfPage';
+// import HalfPage from 'pages/HalfPage/HalfPage';
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
-import { AppWrapper } from './App.styled';
 
-const test = import.meta.env.VITE_API_TEST;
 
 function App() {
-  console.log(test);
   return (
-    <AppWrapper>
+    // <AppWrapper>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
-          <Route path="/first" element={<FirstPage />} />
-          <Route path="/second" element={<SecondPage />}>
-            <Route path=":half" element={<HalfPage />} />
+          <Route path="/catalog" element={<FirstPage />} />
+          <Route path="/favorites" element={<SecondPage />}>
+            {/* <Route path=":half" element={<HalfPage />} /> */}
           </Route>
+          {/* <Route path="*" redirect to element={<ErrorPage />} /> */}
           <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
-    </AppWrapper>
+    // </AppWrapper>
   );
 }
+
+
 export default App;
+
+// У застосунку повинні бути такі маршрути:
+
+// “/” - домашня сторінка з загальним описом послуг, що надає компанія
+
+// “/catalog” - сторінка, що містить каталог камперів різної комплектації
+
+// “/favorites” - сторінка з оголошеннями(оформлення на ваш розсуд), які були додані користувачем в улюблені
+
+// Якщо користувач зайшов за маршрутом, якого не існує, його необхідно перенаправляти на  домашню сторінку.
