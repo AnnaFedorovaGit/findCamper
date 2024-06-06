0eb94d6643da6cab027d8bca63e40dab475a7a8d
AnnaFedorovaGit <ann.petrovska94@gmail.com>
2024-06-06 17:52:23 +0300

    change operations

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -24,7 +24,7 @@ const Filter = () => {
     
   const handleSetType = (event) => {
     const newType = event.target.value;
-    setSelectFilters({ ...selectFilters, type: newType });
+    setSelectFilters({ ...selectFilters, form: newType });
   };
 
   // const handleSetEquipment = (event) => {
